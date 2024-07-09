a2b333148d3927007efe0d8daa177118d5c11324
Zio-4 <philipz4848@gmail.com>
2024-07-08 17:04:35 -0700

    Initial changes: levante fields/options, minor styling changes

--- src/components/CreateAdministrator.vue
+++ src/components/CreateAdministrator.vue
@@ -169,7 +169,6 @@ const submit = async () => {
     orgs.districts = _union(orgs.districts, [_class.districtId]);
     orgs.schools = _union(orgs.schools, [_class.schoolId]);
   }
-  }
 
   await roarfirekit.value
     .createAdministrator(email.value, name, orgs, adminOrgs, isTestData)
