23f5d6b0bcccf978d8a90c80d8357759f4576751
Flávio Conte <flaviohconte@gmail.com>
2025-02-03 13:45:01 -0300

    feature: added download csv button to UI

--- src/components/LEVANTE/RegisterUsersInfo.vue
+++ src/components/LEVANTE/RegisterUsersInfo.vue
@@ -1,11 +1,22 @@
 <template>
   <PvPanel header="Register users">
-    <div class="info-message-container">
+    <div class="info-message-container mb-4">
       <i class="pi pi-exclamation-circle"></i>
       <p>Organizations must be created before registering users. You cannot register users otherwise.</p>
     </div>
-    These fields are <b>REQUIRED</b> for registering users:
-
+    <PvDivider />
+    <div class="mb-4">
+      <h3>CSV Template:</h3>
+      <p>Download and use our CSV template to make sure your data is formatted correctly:</p>
+     <PvButton
+        label="CSV Template"
+        class="bg-primary mb-2 p-3 w-3 text-white border-none border-round h-3rem m-0 hover:bg-red-900"
+        icon="pi pi-download"
+      />
+    </div>   
+    <PvDivider />
+    <h3>Instructions:</h3 >
+    <p>These fields are <b>REQUIRED</b> for registering users:</p>
     <ul>
       <!-- <li><b>id</b> - The unique identifier for the user. Start from 1.</li> -->
       <li><b>userType</b> - The type of user. Must be one of the following: child, parent, or teacher.</li>
@@ -13,19 +24,13 @@
       <li><b>year</b> - The year the user was born.<span class="required">*</span></li>
       <li><b>group OR district AND school</b> - The name of the group or district and school.</li>
     </ul>
-
-    These fields are optional:
-
+    <p>These fields are optional:</p>
     <ul class="optional-fields">
       <li><b>Class</b> - The name of the class.</li>
     </ul>
-
     <p>Parents and Teachers need to have the same organizations as their children / students.</p>
-
-    <p class="mb-6"><span class="required">*</span> = Required only for child users</p>
-
+    <p class="mb-4"><span class="required">*</span> = Required only for child users</p>
     <p>Below is an example of what your CSV/spreadsheet could look like. Only the necessary columns will be processed.</p>
-
     <img
       id="example-image"
       src="https://storage.googleapis.com/road-dashboard/example_researcher_csv.png"
@@ -37,6 +42,8 @@
 
 <script setup>
 import PvPanel from 'primevue/panel';
+import PvDivider from 'primevue/divider';
+import PvButton from 'primevue/button';
 </script>
 
 <style scoped>
