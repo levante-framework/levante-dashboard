7dc10251417e09f9760708fca5fbdaa54a6aff41
Zio-4 <philipz4848@gmail.com>
2025-07-14 15:20:25 -0700

    Adjust cypress config for CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://localhost:5173',
+    // Use HTTPS locally, HTTP in CI
+    baseUrl: process.env.CI ? 'http://localhost:5173' : 'https://localhost:5173',
     supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
