5dbbb64d91469eefe59ff86d28481e0dfb68c7fc
Flávio Conte <flaviohconte@gmail.com>
2025-08-05 12:06:49 -0300

    test: moving the baseUrl inside setupNodeEvents

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,11 +3,12 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   projectId: 'a7nqeq',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on, config) {      
+      // Use HTTPS locally, HTTP in CI
+      config.baseUrl = process.env.CI ? 'http://localhost:5173/' : 'https://localhost:5173/';
+      
+      return config;
     },
-    // Use HTTPS locally, HTTP in CI
-    baseUrl: process.env.CI ? 'http://localhost:5173/' : 'https://localhost:5173/',
     supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
