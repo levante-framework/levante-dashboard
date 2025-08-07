88379d41d565c71084e111678aee07cefdc7d5da
Rodrigo Olivares <rolivares6793@gmail.com>
2025-08-07 17:00:54 -0300

    extracted function to a separate helper

--- src/composables/queries/useUserPermissionsQuery.ts
+++ src/composables/queries/useUserPermissionsQuery.ts
@@ -1,53 +1,26 @@
 import { FIRESTORE_COLLECTIONS } from '@/constants/firebase';
 import { USER_PERMISSIONS_QUERY_KEY } from '@/constants/queryKeys';
+import { mapRolesToPermissions } from '@/helpers/query/permissions';
 import { fetchAllDocuments } from '@/helpers/query/utils';
 import { useAuthStore } from '@/store/auth';
+import { Role } from '@/types';
 import { useQuery, UseQueryOptions, UseQueryReturnType } from '@tanstack/vue-query';
 import { computed } from 'vue';
 
-type UserRoleType = {
-  role: string;
-  siteId: string;
-};
-
-type UserRoleWithPermissionsType = UserRoleType & {
-  permissions: {
-    [action: string]: string[];
-  };
-};
-
-const useUserPermissionsQuery = (userRoles?: UserRoleType[], queryOptions?: UseQueryOptions): UseQueryReturnType => {
+const useUserPermissionsQuery = (userRoles?: Role[], queryOptions?: UseQueryOptions): UseQueryReturnType => {
   const authStore = useAuthStore();
   const userData = authStore?.userData;
-  const roles = userRoles || (userData?.roles as UserRoleType[]);
+  const roles = userRoles || (userData?.roles as Role[]);
 
   const query = useQuery({
     queryKey: [USER_PERMISSIONS_QUERY_KEY, [...roles]],
-    queryFn: async () => fetchAllDocuments(FIRESTORE_COLLECTIONS.PERMISSIONS),
+    queryFn: async () => fetchAllDocuments(FIRESTORE_COLLECTIONS.SYSTEM),
     ...queryOptions,
   });
 
-  const rolesWithPermissions = computed<UserRoleWithPermissionsType[]>(() => {
-    if (!query.data.value) return [];
-
-    const permissionsMap = query.data.value;
-
-    return roles.map(({ siteId, role }: UserRoleType) => {
-      const rolePermissions = permissionsMap[role] || {};
-      const permissionsCopy = Object.entries(rolePermissions).reduce(
-        (acc, [action, actions]) => {
-          acc[action] = [...actions];
-          return acc;
-        },
-        {} as Record<string, string[]>,
-      );
-
-      return {
-        siteId,
-        role,
-        permissions: permissionsCopy,
-      };
-    });
+  const rolesWithPermissions = computed(() => {
+    if (!query?.data?.value?.permissions) return [];
+    return mapRolesToPermissions(roles, query.data.value.permissions);
   });
 
   return { ...query, rolesWithPermissions };
