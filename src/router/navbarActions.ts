34b8ca866e1ece954c25bc691116497156db22fc
Rodrigo Olivares <rolivares6793@gmail.com>
2025-10-31 12:18:24 -0300

    added admin role to the new menu item

--- src/router/navbarActions.ts
+++ src/router/navbarActions.ts
@@ -93,6 +93,7 @@ const navbarActionOptions: Readonly<NavbarAction>[] = [
     title: 'Administrators',
     icon: 'pi pi-users',
     buttonLink: { name: 'Administrators' },
+    requiresAdmin: true,
     requiresSuperAdmin: true,
     project: 'ALL',
     category: 'Users',
