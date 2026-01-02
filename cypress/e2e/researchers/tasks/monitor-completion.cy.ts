e57aa41ed5ad945778869d1cbc1b3c1bc8c290ab
digital-pro <github@proshooters.com>
2026-01-02 09:21:55 -0800

    initial task framework

--- cypress/e2e/researchers/tasks/monitor-completion.cy.ts
+++ cypress/e2e/researchers/tasks/monitor-completion.cy.ts
@@ -1,8 +1,4 @@
-import {
-  ignoreKnownHostedUncaughtExceptions,
-  selectSite,
-  signInWithPassword,
-} from './_helpers';
+import { ignoreKnownHostedUncaughtExceptions, selectSite, signInWithPassword } from '../_helpers';
 
 const email: string = (Cypress.env('E2E_TEST_EMAIL') as string) || 'student@levante.test';
 const password: string = (Cypress.env('E2E_TEST_PASSWORD') as string) || 'student123';
