e2ecd927c3fe95aa2bc64d86a826584023ebfbf8
kgw-wilson <kgw.wilson1@gmail.com>
2026-01-21 22:11:14 -0600

    Store sort option in sessionStorage
    Implement new getInitialSortOption function, update value in storage within onSortChange.

--- src/pages/HomeAdministrator.vue
+++ src/pages/HomeAdministrator.vue
@@ -323,10 +323,28 @@ const sortOptions = ref([
   },
 ]);
 
-// Select default (newest first) by label
-const defaultLabel = 'Start date (descending)';
-const sortKey = ref(sortOptions.value.find(option => option.label === defaultLabel));
 
+const sortOptionStorageKey = 'sortOption';
+
+/**
+ * Get the default sort option based on user's session.
+ * @returns {Object} - The sort option from available sort options
+ */
+const getInitialSortOption = () => {
+
+  const defaultLabel = 'Start date (descending)';
+  const defaultOption = sortOptions.value.find(o => o.label === defaultLabel);
+  const sortOptionFromStorage = sessionStorage.getItem(sortOptionStorageKey);
+
+  if (sortOptionFromStorage) {
+    return JSON.parse(sortOptionFromStorage);
+  } else {
+    sessionStorage.setItem(sortOptionStorageKey, JSON.stringify(defaultOption));
+    return defaultOption;
+  }
+};
+
+const sortKey = ref(getInitialSortOption());
 const sortOrder = ref();
 const sortField = ref();
 const dataViewKey = ref(0);
@@ -379,6 +397,8 @@ const onSortChange = (event) => {
   sortField.value = value[0].field?.fieldPath;
   sortOrder.value = value[0].direction === 'DESCENDING' ? -1 : 1;
   sortKey.value = sortValue;
+
+  sessionStorage.setItem(sortOptionStorageKey, JSON.stringify(sortValue));
 };
 </script>
 
