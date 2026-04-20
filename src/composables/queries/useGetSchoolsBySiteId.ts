60b9f2ae987057717f26dc7d43c4d15e4fd10e91
Rodrigo Olivares <rolivares6793@gmail.com>
2026-04-20 16:51:16 -0300

    add composables for the new queries

---
+++ src/composables/queries/useGetSchoolsBySiteId.ts
@@ -0,0 +1,16 @@
+import { groupsRepository } from '@/firebase/repositories/GroupsRepository';
+import { computeQueryOverrides, QueryOptionsWithEnabled } from '@/helpers/computeQueryOverrides';
+import { useQuery } from '@tanstack/vue-query';
+import { Ref } from 'vue';
+
+export const useGetSchoolsBySiteId = (siteIdRef: Ref<string | null>, queryOptions?: QueryOptionsWithEnabled) => {
+  const queryConditions = [() => !!siteIdRef];
+  const { isQueryEnabled, options } = computeQueryOverrides(queryConditions, queryOptions);
+
+  return useQuery({
+    queryKey: ['schools', siteIdRef],
+    queryFn: async () => await groupsRepository.getSchoolsBySiteId(siteIdRef),
+    enabled: isQueryEnabled,
+    ...options,
+  });
+};
