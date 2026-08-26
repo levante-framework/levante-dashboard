0b216dfbfad1185d9e8891b0f151238a2d06a81d
Rodrigo Olivares <rolivares6793@gmail.com>
2026-08-26 12:46:55 -0300

    add error page when user has no roles

---
+++ src/pages/HomeError.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <div class="flex flex-column align-items-center text-center p-8 px-5">
+      <h2 class="font-semibold m-0">You do not currently belong to any active sites in the dashboard</h2>
+      <p class="m-0 mt-3">Please contact a DCC administrator to be added to an active site</p>
+      <PvButton
+        class="mt-4"
+        icon="pi pi-sign-out"
+        label="Sign out"
+        @click="signOut"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import PvButton from 'primevue/button';
+import useSignOutMutation from '@/composables/mutations/useSignOutMutation';
+
+const { mutate: signOut } = useSignOutMutation();
+</script>
--- src/pages/HomeSelector.vue
+++ src/pages/HomeSelector.vue
@@ -1,6 +1,8 @@
 <template>
   <LevanteSpinner v-if="isLoading" fullscreen data-cy="home-selector-loading" />
 
+  <HomeError v-else-if="userHasNoRoles" />
+
   <HomeAdministrator v-else-if="shouldRenderAdminPage" data-cy="home-selector-admin" />
 
   <HomeParticipant v-else data-cy="home-selector-participant" />
@@ -32,6 +34,7 @@ import { useAuthStore } from '@/store/auth';
 
 const HomeParticipant = defineAsyncComponent(() => import('@/pages/HomeParticipant.vue'));
 const HomeAdministrator = defineAsyncComponent(() => import('@/pages/HomeAdministrator.vue'));
+const HomeError = defineAsyncComponent(() => import('@/pages/HomeError.vue'));
 const ConsentModal = defineAsyncComponent(() => import('@/components/ConsentModal.vue'));
 
 const authStore = useAuthStore();
@@ -61,6 +64,8 @@ const init = () => {
 unsubscribe = authStore.$subscribe(async (_mutation, state) => {
   if (state.roarfirekit?.restConfig) init();
 });
+
+const userHasNoRoles = computed(() => !userData.value?.roles?.length);
 
 const shouldRenderAdminPage = computed(() => {
   const adminRoles = [ROLES.ADMIN, ROLES.RESEARCH_ASSISTANT, ROLES.SITE_ADMIN, ROLES.SUPER_ADMIN];
